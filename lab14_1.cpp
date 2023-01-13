d93925f63048c22585c098c97fed7333dbe9191d
godmonera <borwonpak_du@cmu.ac.th>
2023-01-13 17:09:39 +0700

    finished

--- lab14_1.cpp
+++ lab14_1.cpp
@@ -17,3 +17,66 @@ int main(){
     cout << "\nMin = " << B[5];
     return 0;
 }
+
+void stat(const double A[], int N , double B[]){
+    double sum = 0 , sumsquare=0 , sumt=1 , sumd = 0 ; 
+    int i=0 , j=0 , k=0 , l=0;
+    while(i<N){
+        sum = sum + A[i];
+        i++;
+    }
+
+
+    B[0] = sum/N;
+
+     while(j<N){
+        sumsquare = sumsquare + pow(A[j]-B[0],2);
+        j++;
+    }
+
+    B[1] = sqrt(sumsquare/N);
+
+     while(k<N){
+        sumt = sumt * A[k];
+        k++;
+    }
+
+    B[2] = pow(sumt , 1.0/N );
+
+    
+     while(l<N){
+        sumd = sumd + 1/A[l];
+        l++;
+    }
+
+    B[3] = N/sumd ;
+
+    
+    i=0 ;
+    B[4] = A[0];
+    B[5] = A[0];
+
+    while(i<N){
+
+
+      if(A[i] > B[4]){
+
+        B[4] = A[i] ;
+
+      }
+
+      if(A[i] < B[5]){
+
+        B[5] = A[i] ;
+
+      }
+
+      i++;
+
+    } 
+    
+
+    
+
+
+}
--- lab14_2.cpp
+++ lab14_2.cpp
@@ -21,3 +21,55 @@ int main()
     }while(s != 0 || x != 0 || y != 0);
     return 0;
 }
+
+void updateImage(bool img[][M] ,int s ,int x ,int y ){
+
+    int i , j  ;
+
+    for(i = 0; i< N ; i++){
+        for(j =0 ; j< M ; j++){
+            if( sqrt(pow(i-x,2) + pow(j-y,2)) <= s-1){
+                img[i][j] = 1 ;
+            }
+        }
+    }
+
+
+}
+
+
+void showImage(const bool img[][M]){
+    int i, j;
+
+   for(int o=0; o<M + 2 ; o++){ 
+    cout <<"-";
+   }
+
+
+   cout << "\n";
+
+    for(i=0; i < N ;i++){
+       cout << "|";
+        
+        for(j=0; j < M ;j++){
+
+            if(img[i][j] == true){
+                cout << "*";
+            }else {
+                cout << " ";
+            }
+
+      
+        }
+        cout << "|";
+        cout << "\n";
+    }
+
+    for(int f=0; f<M + 2 ; f++) {
+        cout <<"-";
+        }
+
+    cout << "\n";
+
+
+}
